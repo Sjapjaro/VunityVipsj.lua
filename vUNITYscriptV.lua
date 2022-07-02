919fdbeb6d370e70899ad98a53755cf96c404951
Sjapjaro <108489339+Sjapjaro@users.noreply.github.com>
2022-07-02 14:49:24 +0300

    Update vUNITYscriptV.lua

--- vUNITYscriptV.lua
+++ vUNITYscriptV.lua
@@ -127,7 +127,7 @@ function replaced(repl)
 	ok = true
 end
 
-local Passwords = {"UNITY","UNITY"}
+local Passwords = {"UNITYBEST","UNITYBEST"}
 gg.toast("\n🔒Şifrelenmiş Komut Dosyası🔒")
 local Menu = gg.prompt({"🔰EMİRHAN\n🎬YouTube: EMİRHAN UNITY\n🔒Şifreyi Giriniz:"},{"🛡️Şifreyi Giriniz..."},{"text"}) 
 if not Menu then gg.toast("\n【☑️】Şifreyi Giriniz !!! 【☑️】") os.exit(3) end
